7af320764f29d5e94215f114e8eee04192531f66
Underpig <43320976+underpig1@users.noreply.github.com>
2020-05-03 15:13:40 -1000

    Delete untitled.bat

--- untitled.bat
+++
@@ -1,4 +0,0 @@
-ECHO ON
-REM Execute build file
-SET PATH=%PATH%;C:\Python38
-"C:\Program Files\Blender Foundation\Blender 2.82\blender.exe" C:\Users\chase\OneDrive\Desktop\Blender\NeuroBGE\untitled.blend --python build.py
