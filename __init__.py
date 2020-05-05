4ba25ad087e984d6cf2836a4d411c010dac11bac
Underpig <43320976+underpig1@users.noreply.github.com>
2020-05-04 16:07:09 -1000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,14 @@
+bl_info = {
+    "name": "NeuroBGE",
+    "author": "Underpig",
+    "version": (1, 0),
+    "blender": (2, 80, 0),
+    "description": "Neural node-based game engine",
+    "location": "Logic Editor > Game Engine",
+    "wiki_url": "",
+    "category": "Object",
+}
+
 import bpy
 import neurobge
 
