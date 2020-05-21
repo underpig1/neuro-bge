32a369b8402f35f1da988be77e30a2cb47bc71b0
Underpig <43320976+underpig1@users.noreply.github.com>
2020-05-20 15:39:17 -1000

    Update fetch_file.py

--- collaboration/fetch_file.py
+++ collaboration/fetch_file.py
@@ -1,10 +1,13 @@
-import os
+import subprocess
+import urllib.request
 
 def init():
-  os.system("python -m http.server")
+  return subprocess.Popen(["python -m http.server"], shell = True)
+
+def close_server(server):
+  server.terminate()
 
 def fetch_file(ip, port, from_path, to_path):
-  import urllib.request
   response = urllib.request.urlopen("http://" + ip + ":" + port + from_path[2:])
   file = open(to_path, "wb+")
   file.write(response.read())
