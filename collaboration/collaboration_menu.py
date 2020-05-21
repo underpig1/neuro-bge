e05101d3eb4585ba556174faeef0a2867d46d0d8
Underpig <43320976+underpig1@users.noreply.github.com>
2020-05-20 17:05:21 -1000

    Update collaboration_menu.py

--- collaboration/collaboration_menu.py
+++ collaboration/collaboration_menu.py
@@ -7,5 +7,13 @@ members = [("8 0 0 0", "Member", "Member", "USER")]
 def populate_members(self, context):
     return members
 
-def fetch_file():
-  fetch_file.fetch_file(member, 8000, "C:\\Program Files\\Blender Foundation\\Collaboration\\", bpy.data.filepath)
+def fetch():
+    fetch_file.fetch_file(member, 8000, "C:\\Program Files\\Blender Foundation\\Collaboration\\" + bpy.path.basename(bpy.data.filepath), bpy.data.filepath)
+
+def push():
+    bpy.ops.wm.save_as_mainfile(filepath = "C:\\Program Files\\Blender Foundation\\Collaboration\\" + bpy.path.basename(bpy.data.filepath))
+    fetch_file.init()
+
+def get_host():
+    import socket
+    return socket.gethostbyname(socket.gethostname())
